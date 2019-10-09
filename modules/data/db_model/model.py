6267ccffce9e6770c7135d490682fe40e716442f
usrivastava <utkarsh.srivastava@nexxo.com>
2019-10-09 09:33:30 +0530

    added new models

--- modules/data/db_model/model.py
+++ modules/data/db_model/model.py
@@ -7,6 +7,40 @@ db = Config.get_config_val(key="mongodb", key_1depth="db")
 connect(db, host=url)
 
 
+class User(Document):
+    first_name = StringField(required=True, max_length=100)
+    last_name = StringField(required=True, max_length=100)
+    email = EmailField(required=True)
+    password = StringField(required=True, max_length=100)
+    age = IntField(required=False)
+    gender = StringField(required=False, max_length=1)
+    created_on = ComplexDateTimeField(required=True)
+    telegram_oAuth_token = StringField(required=False, max_length=100)
+
+
+# TODO to use it later. Not yet used.
+class Language(Document):
+    lang_code = StringField(required=True, max_length=8)
+
+
+class Broker(Document):
+    user_id = ReferenceField(User)
+    broker_name = StringField(required=True, max_length=100)
+    default_lang = StringField(required=True, max_length=8, default="en-US")    # TODO replace with Language
+
+
+# Relationship is as below
+# User (1) : Broker (n)
+# Broker (1) : Language (n) : TrainedClassifier(1)
+# So every user can create multiple brokers, each broker may have multiple classifiers, (1 per language)
+class TrainedClassifier(Document):
+    user_id = ReferenceField(User)
+    broker_id = ReferenceField(Broker)
+    model_type = StringField(max_length=100)
+    vector_type = StringField(max_length=100)
+    lang = StringField(required=True, max_length=8)    # TODO replace with Language
+
+
 class Circumstance(EmbeddedDocument):
     input_circumstance = StringField(required=False, max_length=100)
     output_circumstance = StringField(required=False, max_length=100)
@@ -26,7 +60,7 @@ class Response(EmbeddedDocument):
 
 # This is the final class
 class Train(Document):
-    lang = StringField(required=True)
+    lang = StringField(required=True)    # TODO replace with Language
     category = StringField(required=True)
     circumstance = EmbeddedDocumentField('Circumstance')
     variables = ListField(EmbeddedDocumentField('Variables'))
